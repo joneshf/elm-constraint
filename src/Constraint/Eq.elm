2b4bff4af15a6e952364caf79008635f98a870a6
joneshf <jones3.hardy@gmail.com>
2016-06-11 20:15:57 -0700

    Add some derived constraints for `Eq` and `Ord`

--- src/Constraint/Eq.elm
+++ src/Constraint/Eq.elm
@@ -2,6 +2,7 @@ module Constraint.Eq exposing (..)
 
 {-|
 @docs Eq
+@docs eqEither, eqList, eqMaybe
 @docs (===), (=/=)
 -}
 
@@ -30,3 +31,53 @@ Negation of `(===)`.
 (=/=) x y =
   ask <&> \{eq} ->
     not (eq x y)
+
+{-|
+If we have an `Eq a r`, then we can make an `Eq (Maybe a) r`.
+-}
+eqMaybe : Eq a r -> Eq (Maybe a) r
+eqMaybe constraint =
+  { constraint
+  | eq = \x y ->
+    case (x, y) of
+      (Nothing, Nothing) ->
+        True
+      (Just x', Just y') ->
+        constraint.eq x' y'
+      _ ->
+        False
+  }
+
+{-|
+If we have an `Eq a r` and an `Eq b r`, then we can make an `Eq (Result a b) r`.
+-}
+eqEither : Eq a r -> Eq b r -> Eq (Result a b) r
+eqEither aConstraint bConstraint =
+  { aConstraint
+  | eq = \x y ->
+    case (x, y) of
+      (Err x', Err y') ->
+        aConstraint.eq x' y'
+      (Ok x', Ok y') ->
+        bConstraint.eq x' y'
+      _ ->
+        False
+  }
+
+{-|
+If we have an `Eq a r`, then we can make an `Eq (List a) r`.
+-}
+eqList : Eq a r -> Eq (List a) r
+eqList constraint =
+  let equateLists list list' =
+    case (list, list') of
+      ([], []) ->
+        True
+      (x::xs, y::ys) ->
+        constraint.eq x y && equateLists xs ys
+      _ ->
+        False
+  in
+    { constraint
+    | eq = equateLists
+    }
--- src/Constraint/Ord.elm
+++ src/Constraint/Ord.elm
@@ -2,7 +2,11 @@ module Constraint.Ord exposing (..)
 
 {-|
 @docs Ord
+@docs ordEither, ordList, ordMaybe
+@docs max, min
 -}
+
+import Constraint exposing (..)
 
 {-|
 Generalized ordering.
@@ -11,3 +15,87 @@ type alias Ord a r =
   { r
   | compare : a -> a -> Order
   }
+
+{-|
+Find the larger of two values.
+-}
+max : a -> a -> Constraint (Ord a r) a
+max x y =
+  ask <&> \c ->
+    case c.compare x y of
+      GT ->
+        x
+      _ ->
+        y
+
+{-|
+Find the smaller of two values.
+-}
+min : a -> a -> Constraint (Ord a r) a
+min x y =
+  ask <&> \c ->
+    case c.compare x y of
+      LT ->
+        x
+      _ ->
+        y
+
+{-|
+If we have an `Ord a r`, then we can make an `Ord (Maybe a) r`.
+-}
+ordMaybe : Ord a r -> Ord (Maybe a) r
+ordMaybe constraint =
+  { constraint
+  | compare = \x y ->
+    case (x, y) of
+      (Nothing, Nothing) ->
+        EQ
+      (Nothing, Just _) ->
+        LT
+      (Just _, Nothing) ->
+        GT
+      (Just x', Just y') ->
+        constraint.compare x' y'
+  }
+
+{-|
+If we have an `Ord a r` and an `Ord b r`, then we can make an `Ord (Result a b) r`.
+-}
+ordEither : Ord a r -> Ord b r -> Ord (Result a b) r
+ordEither aConstraint bConstraint =
+  { aConstraint
+  | compare = \x y ->
+    case (x, y) of
+      (Err x', Err y') ->
+        aConstraint.compare x' y'
+      (Err _, Ok _) ->
+        LT
+      (Ok _, Err _) ->
+        GT
+      (Ok x', Ok y') ->
+        bConstraint.compare x' y'
+  }
+
+{-|
+If we have an `Ord a r`, then we can make an `Ord (List a) r`.
+-}
+ordList : Ord a r -> Ord (List a) r
+ordList constraint =
+  let compareLists list list' =
+    case (list, list') of
+      ([], []) ->
+        EQ
+      ([], _) ->
+        LT
+      (_, []) ->
+        GT
+      (x::xs, y::ys) ->
+        case constraint.compare x y of
+          EQ ->
+            compareLists xs ys
+          ordering ->
+            ordering
+  in
+    { constraint
+    | compare = compareLists
+    }
